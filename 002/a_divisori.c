ed892892435fb5fd193722f7a49f2a4817b58f92
Hubertste1 <114228266+Hubertste1@users.noreply.github.com>
2022-12-08 23:53:43 +0100

    Create a_divisori.c

---
+++ 002/a_divisori.c
@@ -0,0 +1,26 @@
+/** 
+* \mainpage a_divisori.c
+*
+* @brief Scrivere un programma che, richiesto un numero intero, visualizzi tutti i suoi divisori.
+* 
+* @author Hubert Stefanski
+* @date 06/12/2022
+*/
+
+#include <stdio.h>		                        //includo i comandi 
+
+int main() 
+{
+	printf("-Esercizio A Hubert Stefanski-\n");
+	
+	int n, a;			                        //dichiaro le variabili
+	
+	printf("Inserisci un numero: ");	        //richiesta input del numero
+	scanf("%d", &n);
+	
+	for(a = 2; i < n; a++)		                //itero tutti gli interi da 2 al numero inserito
+	{
+		if(n%a==0)								//controllo se l'indice e' un divisore
+			printf("%d e' un divisore\n", i);		
+	}	
+}
