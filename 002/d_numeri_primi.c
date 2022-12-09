77c5c7b57bb000a07012e00301ea3210bd10cbad
Hubertste1 <114228266+Hubertste1@users.noreply.github.com>
2022-12-10 00:00:51 +0100

    Update d_numeri_primi.c

--- 002/d_numeri_primi.c
+++ 002/d_numeri_primi.c
@@ -2,8 +2,8 @@
 * \mainpage d_numeri_primi.c
 *
 * @brief Scrivere un programma che stampi tutti i numeri primi compresi tra 1 e 100. 
-*        Un numero è primo quando ha come divisore uno e sé stesso. Quindi è primo ciascun 
-*        numero naturale maggiore di 1 che sia divisibile solamente per 1 e per sé stesso.
+*        Un numero Ã¨ primo quando ha come divisore uno e sÃ© stesso. Quindi Ã¨ primo ciascun 
+*        numero naturale maggiore di 1 che sia divisibile solamente per 1 e per sÃ© stesso.
 * 
 * @author Hubert Stefanski
 * @date 06/12/2022
@@ -15,22 +15,22 @@ int main()
 {
     printf("-Esercizio D Hubert Stefanski-\n");
 
-    //diachiaro le variabili - c=variabile di controllo inizializzata a vero
+    //diachiaro le variabili 
     int a, b, c=1;        
 
-    for(a=2; a<101; a++) 
-	{       //itero tutti i numeri dal 2 al 100
+    for(a=2; a<101; a++) //tutti i numeri dal 2 al 100
+	{       
         
-        for(b = 2; b<a; b++) 
-		{      //itero tutti i numeri dal 2 all'indice del primo ciclo
+        for(b = 2; b<a; b++) //tutti i numeri dal 2 all'indice del primo ciclo
+		{      
         	
-            //se i1 è divisibile per un numero diverso da se stesso o 1 metto la variabile di controllo a falso
-            if(a % b == 0) 
+            
+            if(a % b == 0) //se i1 Ã¨ divisibile per un numero diverso da se stesso o 1 metto la variabile di controllo a falso
                 c=0;          
             
         }
 
-        //stampo il numero solo se la varibile di controllo è su vero
+        //stampo il numero solo se la varibile di controllo Ã¨ su vero
         if(c) 
             printf("%d e' un numero primo \n", a);
         
