81ab5b9ff82c1a2df10976b8d2c386bf2d4397b4
Hubertste1 <114228266+Hubertste1@users.noreply.github.com>
2022-12-10 00:01:58 +0100

    Update e_fattoriali.c

--- 002/e_fattoriali.c
+++ 002/e_fattoriali.c
@@ -3,35 +3,35 @@
 *
 * @brief Si scriva un programma che calcoli il fattoriale di un numero intero N dato dalla 
 *        tastiera. Si ricordi che il fattoriale di un numero n (simbolo n!) viene calcolato 
-*        con la seguente formula: n! = n ·(n–1)·(n–2)· ... ·2 ·1.
+*        con la seguente formula: n! = n Â·(nâ€“1)Â·(nâ€“2)Â· ... Â·2 Â·1.
 * 
 * @author Hubert Stefanski
 * @date 06/12/2022
 */
 
-#include <stdio.h>                               //inclusione librerie
+#include <stdio.h>                              	 //inclusione librerie
 
 int main()  
 {
     printf("-Esercizio E Hubert Stefanski-\n");
 
 
-    int n, i;                                   //diachiaro le variabili       
+    int n, i;                                  		 //diachiaro le variabili       
 
-    printf("Inserisci un numero: ");            //richiesta numero
+    printf("Inserisci un numero: ");          		 //richiesta numero
     scanf("%d", &n);
 
     if(n>0) 
-		{                                       //controllo se il numero è positivo
+		{                                        //controllo se il numero Ã¨ positivo
         for(i=n-1; i>0; i--) 
 		{
-            n *= i;                             //moltiplico il numero per tutti i numeri minori di esso
+            n *= i;                           		 //moltiplico il numero per tutti i numeri minori di esso
         }
 
         printf("\nIl fattoriale del numero inserito e' %d\n\n", n);     //stampa risultato
     }
     else 
 	{
-        printf("\nNon e' possibile calcolare il fattoriale di un numero negativo\n\n");     //eccezione numero negativo
+        printf("\nNon e' possibile calcolare il fattoriale di un numero negativo\n\n");     //no numero negativo
     }
 }
